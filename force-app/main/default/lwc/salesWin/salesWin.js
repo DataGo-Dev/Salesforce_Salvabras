4e56f867fa4378dbe47af25ddf709612ed334e9f
João Vitor <joaovitoruser@gmail.com>
2024-10-28 12:00:32 -0300

    add

--- force-app/main/default/lwc/salesWin/salesWin.js
+++ force-app/main/default/lwc/salesWin/salesWin.js
@@ -1,3 +1,20 @@
 import { LightningElement, api } from "lwc";
+import getPricebooks from '@salesforce/apex/SalesWin.getPricebooks';
+
 export default class SalesWin extends LightningElement {
+
+    @api recordId; pricebooks; values = {};   
+    async connectedCallback(){        
+        this.pricebooks = await getPricebooks({recordId: this.recordId});
+    }
+    handleSelectValue(event){
+        const value =  event.detail.value || event.target.value; 
+        const key = event.currentTarget.dataset.key;
+        this.values[key] = value;        
+    }
+
+
+
+
+
 }
