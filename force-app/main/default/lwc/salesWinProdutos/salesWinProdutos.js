0e31ea05efba119b0efe604eb71774c64911d8de
João Vitor <joaovitoruser@gmail.com>
2024-10-29 16:38:00 -0300

    search index

--- force-app/main/default/lwc/salesWinProdutos/salesWinProdutos.js
+++ force-app/main/default/lwc/salesWinProdutos/salesWinProdutos.js
@@ -3,7 +3,7 @@ import { LightningElement, api, track } from "lwc";
 
 export default class SalesWinProdutos extends LightningElement {
     @api recordId;
-    term = ""; searchTimeout; @track produtos;
+    term = ""; searchTimeout; @track produtos; selectedIndex = -1; 
 
     handleInputChange(event) {
 		this.produtos = undefined;
@@ -19,7 +19,8 @@ export default class SalesWinProdutos extends LightningElement {
     }
 
     async handleSearch() {
-      
+		
+		this.selectedIndex = -1;
 		const prods = await searchProducts({
 			recordId: this.recordId,
 			term: this.term
@@ -32,4 +33,51 @@ export default class SalesWinProdutos extends LightningElement {
 		
 		this.produtos = prods;
     }
+
+	handleKeyDown(event) {
+
+        const { key } = event;
+
+        if (key === 'ArrowDown') {
+            event.preventDefault(); 
+            if (this.selectedIndex < this.produtos.length - 1) {
+                this.selectedIndex++;
+                this.updateSelection();
+            }
+        } else if (key === 'ArrowUp') {
+            event.preventDefault();
+            if (this.selectedIndex > 0) {
+                this.selectedIndex--;
+                this.updateSelection();
+            }
+        } else if (key === 'Enter') {
+            event.preventDefault();
+            if (this.selectedIndex > -1) {
+			   const selectedItem = this.produtos[this.selectedIndex];
+			   const evt ={ 
+					currentTarget: { 
+						dataset: { id: selectedItem.Id} 
+					} 
+				}
+               this.hanldleClickItem(evt);
+            }
+        }
+    }
+
+    updateSelection() {
+        const items = this.template.querySelectorAll('.itemBusca');
+        items.forEach((item, index) => {
+            const isSelected = index === this.selectedIndex;
+            item.classList.toggle('selected', isSelected); 
+            
+            if (isSelected) {
+                item.scrollIntoView({ block: 'nearest', behavior: 'smooth' });
+            }
+        });
+    }
+
+	hanldleClickItem(event){
+		this.term = '';
+		this.produtos = undefined;
+	}
 }
