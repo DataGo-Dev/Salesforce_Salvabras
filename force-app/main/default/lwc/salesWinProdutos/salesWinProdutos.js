96407fa9b461a817fafd55f15e02d14bf216c175
João Vitor <joaovitoruser@gmail.com>
2024-10-29 16:25:05 -0300

    find

--- force-app/main/default/lwc/salesWinProdutos/salesWinProdutos.js
+++ force-app/main/default/lwc/salesWinProdutos/salesWinProdutos.js
@@ -1,6 +1,35 @@
-import { LightningElement, api } from "lwc";
-export default class SalesWinProdutos extends LightningElement {
-	@api recordId;
+import searchProducts from "@salesforce/apex/SalesWinProdutos.searchProducts";
+import { LightningElement, api, track } from "lwc";
 
-	
+export default class SalesWinProdutos extends LightningElement {
+    @api recordId;
+    term = ""; searchTimeout; @track produtos;
+
+    handleInputChange(event) {
+		this.produtos = undefined;
+
+        this.term = event.target.value.trim();
+        clearTimeout(this.searchTimeout);
+
+        if (this.term.length >= 2) {
+            this.searchTimeout = setTimeout(() => {
+                this.handleSearch();
+            }, 300);
+        }
+    }
+
+    async handleSearch() {
+      
+		const prods = await searchProducts({
+			recordId: this.recordId,
+			term: this.term
+		});
+
+		if(prods.length === 0) {
+			this.produtos = undefined;
+			return;
+		}
+		
+		this.produtos = prods;
+    }
 }
