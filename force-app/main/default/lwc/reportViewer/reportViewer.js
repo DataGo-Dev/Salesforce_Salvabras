b8f2359ad87c46b2d58da47f71a290ad067ca6c0
manoel27 <ma201800927@gmail.com>
2024-12-11 17:30:01 -0300

    add

---
+++ force-app/main/default/lwc/reportViewer/reportViewer.js
@@ -0,0 +1,50 @@
+import { LightningElement, api } from 'lwc';
+import savePdf from '@salesforce/apex/QuoteService.savePdf';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class ReportViewer extends LightningElement {
+
+    @api recordId;
+    url;
+    loading = false;
+
+    connectedCallback(){
+        this.url = `${window.location.origin}/apex/QuoteReport?id=${this.recordId}`;
+    }
+
+    async savePdf(){
+
+        this.loading = true;
+        const resp = await savePdf({
+            recordId: this.recordId,
+            reportName: 'QuoteReport',
+            report: 'COTAÇÃO'
+        });
+
+        this.loading = false;
+        if(resp){
+            this.closeAction();
+            this.showToast('PDF criado com sucesso!', 'success');
+        }
+      
+
+    }
+
+    showToast(message, variant) {
+        const event = new ShowToastEvent({
+            message: message,
+            variant: variant,
+            mode: 'dismissable'
+        });
+        this.dispatchEvent(event);
+    }
+
+    closeAction() {
+        const closeModalEvent = new CustomEvent("modalclose");
+        this.dispatchEvent(closeModalEvent);
+    }
+
+    
+
+
+}
